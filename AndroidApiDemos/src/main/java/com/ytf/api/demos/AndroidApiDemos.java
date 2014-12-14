35f3433d82f90c5f0318917ee83fa26ac0ab70a9
Skywalker <andyhua2014@gmail.com>
2014-12-14 20:54:21 +0800

    Initial commit

---
+++ AndroidApiDemos/src/main/java/com/ytf/api/demos/AndroidApiDemos.java
@@ -0,0 +1,76 @@
+package com.ytf.api.demos;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AndroidApiDemos extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        String path = intent.getStringExtra("com.example.android.apis.Path");
+
+        if (path == null) {
+            path = "";
+        }
+
+        setListAdapter(new SimpleAdapter(this, getData(path), android.R.layout.simple_list_item_1,
+                new String[]{"title"}, new int[]{android.R.id.text1}));
+        getListView().setTextFilterEnabled(true);
+    }
+
+    /**
+     * @param prefix
+     * @return
+     */
+    protected List<Map<String, Object>> getData(String prefix) {
+        List<Map<String, Object>> myData = new ArrayList<Map<String, Object>>();
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_SAMPLE_CODE);
+
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(mainIntent, 0);
+
+        if (null == list) {
+            return myData;
+        }
+
+        String[] prefixPath;
+        String prefixWithSlash = prefix;
+
+        if (prefix.equals("")) {
+            prefixPath = null;
+        } else {
+            prefixPath = prefix.split("/");
+            prefixWithSlash = prefix + "/";
+        }
+
+        int len = list.size();
+        Map<String, Boolean> entries = new HashMap<String, Boolean>();
+
+        for (int i = 0; i < len; ++i){
+            ResolveInfo info = list.get(i);
+            CharSequence labelSeq = info.loadLabel(pm);
+            String label = labelSeq != null ? labelSeq.toString() : info.activityInfo.name;
+
+            if (prefixWithSlash.length() == 0 || label.startsWith(prefixWithSlash)){
+
+            }
+        }
+
+        return myData;
+    }
+}
---
+++ AndroidApiDemos/src/main/java/com/ytf/api/demos/ApiDemosApplication.java
@@ -0,0 +1,20 @@
+package com.ytf.api.demos;
+
+import android.app.Application;
+
+/**
+ * Package: com.ytf.api.demos
+ * Created with AndroidApiDemos
+ * User: WuHua
+ * Date: 14-12-13
+ * Time: 17:40
+ * Description:
+ */
+public class ApiDemosApplication extends Application{
+    private static final String TAG = "ApiDemosApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
